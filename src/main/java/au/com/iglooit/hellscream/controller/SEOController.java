465ddf5b8a07b8495c2e68dca5086c8c40dbf6d8
Nicholas Zhu <nicholas.zhu@fairfaxmedia.com.au>
2014-11-10 13:35:44 +1100

    email template

--- src/main/java/au/com/iglooit/hellscream/controller/SEOController.java
+++ src/main/java/au/com/iglooit/hellscream/controller/SEOController.java
@@ -6,13 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Locale;
 
 /**
  * Created with IntelliJ IDEA.
